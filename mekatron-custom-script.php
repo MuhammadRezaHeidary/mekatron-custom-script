29a5a808fcf782146f295960f872cb5a8810e79b
MuhammadRezaHeidary <muhammadreza.heidary@gmail.com>
2023-07-02 19:37:21 +0330

    Init Commit

---
+++ mekatron-custom-script.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Mekatron Custom Script
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Custom HTML/CSS/JS Script
+Version: 1.0
+Author: Muhammmad Reza Heidary
+Author URI: https://mekatronik.ir/
+License: MIT
+*/
